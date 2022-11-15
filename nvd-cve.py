41fb5cce88774edac4907bee9dc2c59293d8496a
Vincent Danen <vdanen@annvix.com>
2022-11-15 12:27:27 -0700

    some cleanups

--- nvd-cve.py
+++ nvd-cve.py
@@ -4,7 +4,6 @@ import argparse
 import datetime
 import gzip
 import os
-import sys
 import json
 import urllib.request
 
@@ -12,7 +11,8 @@ start_year   = 1999
 #current_year = 2004
 current_year = int(datetime.datetime.today().strftime('%Y'))
 
-class CVE():
+
+class CVE:
     def __init__(self, cve_entry):
         self.cve_dict = {}
         for key, value in cve_entry.items():
@@ -41,8 +41,8 @@ class CVE():
 
 def download(url, localfile):
     # download an individual file
-    if not '/' in url:
-        print ('No URL provided!')
+    if '/' not in url:
+        print('No URL provided!')
         return None
 
     print(f'Downloading {url}...')
@@ -75,9 +75,8 @@ def download_gzips():
         if os.path.isfile(gzfile):
             # don't download if we've already downloaded today
             dt_now = datetime.datetime.now()
-            dt_mod = datetime.datetime.fromtimestamp(os.path.getmtime(gzfile))
             dt_cre = datetime.datetime.fromtimestamp(os.path.getctime(gzfile))
-            if ((dt_now - dt_cre).total_seconds() > 60*60*24):
+            if (dt_now - dt_cre).total_seconds() > 60*60*24:
                 print(f'{gzfile} is older than 24h, refreshing...')
                 dl_file = download(f'{hosturl}{gzfile}', gzfile)
                 if dl_file is not None:
@@ -87,7 +86,7 @@ def download_gzips():
             if dl_file is not None:
                 downloaded.append(gzfile)
 
-    return(years)
+    return years
 
 
 def parse_nvd(gzfile):
@@ -99,7 +98,7 @@ def parse_nvd(gzfile):
     with gzip.open(gzfile, 'rb') as f:
 #        print(f'Importing records from {gzfile}')
         j = json.loads(f.read())
-        records = len(j['CVE_Items'])
+#        records = len(j['CVE_Items'])
 #        print(f'Number of CVE entries loaded: {records}')
 
         cve_entries = [CVE(cve_dict) for cve_dict in j['CVE_Items']]
@@ -109,7 +108,8 @@ def parse_nvd(gzfile):
 
 def main():
     parser = argparse.ArgumentParser(description='NVD parsing tool')
-    parser.add_argument('--year-stats', dest='year_stats', action='store_true', default=False, help='Display CVE count by year')
+    parser.add_argument('--year-stats', dest='year_stats', action='store_true', default=False,
+                        help='Display CVE count by year')
 
     args = parser.parse_args()
 
@@ -132,7 +132,6 @@ def main():
                 if str(y) in c.publishedDate:
                     x += 1
             print(f'  {y}: {x}')
-
 
 
 if __name__ == '__main__':
